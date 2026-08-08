e0c27563503bbdc67cc2cbe0407f1139cacbacd5
Mustafa Fares <mustaphafares445@gmail.com>
2026-08-08 15:27:55 +0300

    Use typeahead selectors for product category and master product
    Replace the product category dropdown with flutter_typeahead, add searchable master product selection with images, and include masterProductId when creating products.

--- lib/features/products/domain/usecases/add_product_use_case.dart
+++ lib/features/products/domain/usecases/add_product_use_case.dart
@@ -28,6 +28,8 @@ class AddProductParams with Params {
   BodyMap getBody() => {
     "storeId": 1,
     "categoryId": params.categoryId,
+    if (params.masterProductId != null)
+      "masterProductId": params.masterProductId,
     "name": params.title,
     if (params.description != null) "description": params.description,
     "sourceType": "manual",
