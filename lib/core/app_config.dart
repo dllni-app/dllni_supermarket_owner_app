1f8b37762b0a6b3a20b7b5a780bc753d021b2788
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-11 13:36:52 +0300

    Update base URL to dllni.mustafafares.com

--- lib/core/app_config.dart
+++ lib/core/app_config.dart
@@ -3,6 +3,6 @@ class AppConfig {
 
   static const String orgIdentifier = 'com.dllni.supermarket_owner_app';
   static const String baseUrl =
-      'https://alnadha.net';// 'https://alnadha.net'; //https://dllni.mustafafares.com
+      'https://dllni.mustafafares.com';// 'https://alnadha.net'; //https://dllni.mustafafares.com
   const AppConfig._();
 }
