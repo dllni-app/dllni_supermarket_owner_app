e9b0ec836fbd49e65270bb9acd584f89ea1fb2fa
AHMADMalbetar <107564019+AHMADMalbetar@users.noreply.github.com>
2026-08-15 22:18:52 +0300

    Add supermarket notification item events

--- lib/features/home/view/manager/bloc/home_event.dart
+++ lib/features/home/view/manager/bloc/home_event.dart
@@ -4,63 +4,63 @@ abstract class HomeEvent {}
 
 class GetDashboardOverviewEvent extends HomeEvent {
   final GetDashboardOverviewParams params;
-
   GetDashboardOverviewEvent({required this.params});
 }
 
 class GetNewOrdersEvent extends HomeEvent with EventWithReload {
   final GetNewOrdersParams params;
-
   @override
   final bool isReload;
-
   GetNewOrdersEvent({required this.params, this.isReload = false});
 }
 
 class GetPreparingOrdersEvent extends HomeEvent with EventWithReload {
   final GetPreparingOrdersParams params;
-
   @override
   final bool isReload;
-
   GetPreparingOrdersEvent({required this.params, this.isReload = false});
 }
 
 class RejectOrderEvent extends HomeEvent {
   final RejectOrderParams params;
-
   RejectOrderEvent({required this.params});
 }
 
 class GetDailyCountEvent extends HomeEvent {
   final GetDailyCountParams params;
-
   GetDailyCountEvent({required this.params});
 }
 
 class AcceptOrderEvent extends HomeEvent {
   final AcceptOrderParams params;
-
   AcceptOrderEvent({required this.params});
 }
 
 class GetPerformanceReportEvent extends HomeEvent {
   final GetPerformanceReportParams params;
-
   GetPerformanceReportEvent({required this.params});
 }
 
 class FetchNotificationsEvent extends HomeEvent with EventWithReload {
   final FetchNotificationsParams params;
-
   @override
   final bool isReload;
-
   FetchNotificationsEvent({required this.params, this.isReload = false});
 }
 
 class MakeReadAllNotificationsEvent extends HomeEvent {
   final MakeReadAllNotificationsParams params;
-
   MakeReadAllNotificationsEvent({required this.params});
 }
+
+class MakeReadNotificationEvent extends HomeEvent {
+  final String id;
+  MakeReadNotificationEvent({required this.id});
+}
+
+class DeleteNotificationEvent extends HomeEvent {
+  final String id;
+  DeleteNotificationEvent({required this.id});
+}
+
+class DeleteAllNotificationsEvent extends HomeEvent {}
